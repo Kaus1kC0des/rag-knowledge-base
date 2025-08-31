8296893e55c0f8764dfc1cd8955ecbfb8079f26b
Kausik D <kausikdevanathan@gmail.com>
2025-08-31 10:50:29 +0530

    Optimised imports

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,9 +1,5 @@
-from sqlalchemy.orm import Session
-from fastapi import HTTPException
-import logging
-
-from .authenticate_user import  authenticate_user
+from .authenticate_user import authenticate_user
 from .get_pg_database import get_pg_db
 from .get_mongo_database import get_mongo_db
-
-__all__ = ["authenticate_user", "get_pg_db", "get_mongo_db"]
+from .get_redis_db import get_redis_db
+__all__ = ["authenticate_user", "get_pg_db", "get_mongo_db", "get_redis_db"]
